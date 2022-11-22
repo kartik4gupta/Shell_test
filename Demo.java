cfc26b81c68baccbe13188cbf71497e7ea61effc
kartik4gupta <kartik4gupta@gmail.com>
2022-11-22 01:00:00 +0100

    yyyyyyyy???

---
+++ Demo.java
@@ -0,0 +1,43 @@
+package com.gamefromscratch;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class Demo extends Game {
+	SpriteBatch batch;
+
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new Menu(this));
+	}
+	@Override
+	public void render () {
+		super.render();
+
+	}
+	
+	@Override
+	public void dispose () {
+		super.dispose();
+		batch.dispose();
+//		img_entry_screen.dispose();
+	}
+}
